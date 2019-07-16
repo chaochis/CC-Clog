23929a31da89844fe8000500cd35316a13ca2eb8
chao chis <1916569889@qq.com>
2019-07-16 15:34:51 +0800

    前端架构调整

---
+++ front/admin/src/components/cclogin.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="cclogin">
+        <span class="cc-login-text">登录</span><el-divider direction="vertical"></el-divider><span class="cc-login-text">注册</span>
+    </div>
+</template>
+<script>
+export default {
+    name: "cclogin",
+    data(){
+        return {};
+    }
+}
+</script>
+
+<style scoped>
+
+#cclogin >>> .cc-login-text{
+    color: red;
+}
+
+</style>
--- front/admin/src/components/ccnav.vue
+++ front/admin/src/components/ccnav.vue
@@ -2,22 +2,29 @@
     <div id="cc-nav">
         <el-menu mode="horizontal">
             <!-- <el-menu-item><el-image :src="logoSrc" style="width: 60px;height: 100px;"></el-image></el-menu-item> -->
-            <el-menu-item><span class="cc-nav-logo-text">CC-Clog</span></el-menu-item>
-            <el-menu-item class="cc-nav-search-area"><el-input placeholder="请输入搜索内容"></el-input></el-menu-item>
+            <el-menu-item><span class="cc-nav-logo-text">CC-Clog Admin</span></el-menu-item>
+            <el-menu-item class="cc-nav-search-area"><el-input placeholder="请输入搜索内容" @change="onSearch" v-model="searchText"></el-input></el-menu-item>
             <el-menu-item>首页</el-menu-item>
-            <el-menu-item>产品区</el-menu-item>
             <el-menu-item>讨论区</el-menu-item>
-            <el-menu-item class="cc-nav-login-area"><span>登录</span><el-divider direction="vertical"></el-divider><span>注册</span></el-menu-item>
+            <el-menu-item>产品</el-menu-item>
+            <el-menu-item class="cc-nav-login-area"><cclogin></cclogin></el-menu-item>
         </el-menu>
     </div>
 </template>
 <script>
+import cclogin from './cclogin'
 export default {
     name: 'cc-nav',
+    components: {cclogin},
     data(){
         return {
-            
+            searchText: ""
         };
+    },
+    methods: {
+        onSearch(){
+            alert("1")
+        }
     }
 }
 </script>
@@ -25,10 +32,6 @@ export default {
 .cc-nav-logo-text{
     color: #665544;
     font-size: 20px;
-}
-
-#cc-nav >>> .cc-nav-login-area{
-    color: red;
 }
 
 #cc-nav >>> .cc-nav-search-area{
